5b96f4230f15e98d761b3482d199156e360a8f37
Riz <56162066+riz-l@users.noreply.github.com>
2020-02-28 00:54:22 +0000

    Riz edit added

---
+++ assets/js/app.js
@@ -0,0 +1,19 @@
+// // Getting data // TODO
+// db.collection("collectionName")
+//   .where("fieldName", "==", "")
+//   .get()
+//   .then(snapshot => {
+//     snapshot.docs.forEach(doc => {
+//       renderCollectionName(doc);
+//     });
+//   });
+
+// // Ordering data // TODO
+// db.collection("collectionName")
+//   .orderBy("name")
+//   .get()
+//   .then(snapshot => {
+//     snapshot.docs.forEach(doc => {
+//       renderCollectionName(doc);
+//     });
+//   });

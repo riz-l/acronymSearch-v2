630b6603c4f55d4029fcea4f05920077fe754ab9
Riz <56162066+riz-l@users.noreply.github.com>
2020-03-05 00:49:20 +0000

    Added updates w/Ji code

--- sw.js
+++ sw.js
@@ -1,11 +1,9 @@
-// Service Worker Version == v2.2.1
-// e18: https://www.youtube.com/watch?v=ChXgikdQJR8
+// e23: https://www.youtube.com/watch?v=ZfL61cOUImw
 
 // Static Cache
-const staticCacheName = "site-static-v1.3";
-
+const staticCacheName = "site-static-v5.1";
 // Dynamic Cache
-const dynamicCache = "site-dynamic-v1.3";
+const dynamicCacheName = "site-dynamic-v5.1";
 
 // Cache Assets
 const assets = [
@@ -20,13 +18,25 @@ const assets = [
   "assets/img/sponsors/dxc-black.png",
   "assets/img/sponsors/leonardo-logo.svg",
   "assets/img/icons/tdi.ico",
-  "https://kit.fontawesome.com/78b1d29e85.js"
+  "https://kit.fontawesome.com/78b1d29e85.js",
+  "https://fonts.googleapis.com/css?family=Roboto:500|Poppins:600&display=swap",
+  "pages/fallback.html"
 ];
 
-// Install Service Worker
+// Limit cache size function
+const limitCacheSize = (name, size) => {
+  caches.open(name).then(cache => {
+    cache.keys().then(keys => {
+      if (keys.length > size) {
+        cache.delete(keys[0]).then(limitCacheSize(name, size));
+      }
+    });
+  });
+};
+
+// Install service worker
 self.addEventListener("install", evt => {
-  // Console log to instruct that sw.js has been installed
-  //   console.log('service worker has been installed');
+  // console.log('service worker installed');
   evt.waitUntil(
     caches.open(staticCacheName).then(cache => {
       console.log("caching shell assets");
@@ -35,38 +45,45 @@ self.addEventListener("install", evt => {
   );
 });
 
-// Listen for Service Worker Activation
+// Activate service worker
 self.addEventListener("activate", evt => {
-  // Console log to intercept fetch resource request
-  //   console.log("service worker has been activated");
+  // console.log('service worker activated');
   evt.waitUntil(
     caches.keys().then(keys => {
-      //   console.log(keys);
+      // console.log(keys);
       return Promise.all(
         keys
-          .filter(key => key !== staticCacheName)
+          .filter(key => key !== staticCacheName && key !== dynamicCacheName)
           .map(key => caches.delete(key))
       );
     })
   );
 });
 
-// Fetch event
+// Fetch event - get cached
 self.addEventListener("fetch", evt => {
-  // Console log to intercept fetch resource request
-  //   console.log('fetch event', evt);
+  // console.log('fetch event', evt);
   evt.respondWith(
-    caches.match(evt.request).then(cacheRes => {
-      // Return from cache is item is in assets, otherwise continue with normal fetch request
-      return (
-        cacheRes ||
-        fetch(evt.request).then(fetchRes => {
-          return caches.open(dynamicCache).then(cache => {
-            cache.put(evt.request.url, fetchRes.clone());
-            return fetchRes;
-          });
-        })
-      );
-    })
+    caches
+      .match(evt.request)
+      .then(cacheRes => {
+        return (
+          cacheRes ||
+          fetch(evt.request).then(fetchRes => {
+            return caches.open(dynamicCacheName).then(cache => {
+              cache.put(evt.request.url, fetchRes.clone());
+              // Limits 'dynamicCacheName' cached items size
+              limitCacheSize(dynamicCacheName, 15);
+              return fetchRes;
+            });
+          })
+        );
+      })
+      // If .html file fails to load, 'fallback.html' is returned instead
+      .catch(() => {
+        if (evt.request.url.indexOf(".html") > -1) {
+          return caches.match("/pages/fallback.html");
+        }
+      })
   );
 });

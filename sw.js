15b1396c1224361dc4ebdbd7ea228a1fb422c710
Xiangj1030 <57798370+Xiangj1030@users.noreply.github.com>
2020-03-07 16:33:08 +0000

    simplify design and background
    fixed button border/background div

--- sw.js
+++ sw.js
@@ -1,32 +1,38 @@
-// Service Worker Version == v2.2.1
-// e18: https://www.youtube.com/watch?v=ChXgikdQJR8
-
-// Static Cache
-const staticCacheName = "site-static-v1";
-
-// Dynamic Cache
-const dynamicCache = "site-dynamic-v1";
-
-// Cache Assets
+const staticCacheName = "site-static-v2";
+const dynamicCacheName = "site-dynamic-v2";
 const assets = [
-  "index.html",
-  "assets/js/app.js",
-  "assets/js/bootstrap.min.js",
-  "assets/js/ui.js",
-  "assets/css/boostrap.min.css",
-  "assets/css/sidebar.css",
-  "assets/css/style.css",
-  "assets/img/tdi-logo.png",
-  "assets/img/sponsors/dxc-black.png",
-  "assets/img/sponsors/leonardo-logo.svg",
-  "assets/img/icons/tdi.ico",
-  "https://kit.fontawesome.com/78b1d29e85.js"
+  "/",
+  "/index.html",
+  "/assets/js/app.js",
+  "/assets/js/ui.js",
+  "/assets/js/bootstrap.min.js",
+  "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js",
+  "/assets/css/style.css",
+  "/assets/css/bootstrap.min.css",
+  "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css",
+  "/assets/css/sidebar.css",
+  "/assets/img/tdi-logo.png",
+  "https://fonts.googleapis.com/css?family=Roboto:500|Poppins:600&display=swap",
+  "https://code.jquery.com/jquery-3.4.1.slim.min.js",
+  "https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js",
+  "https://kit.fontawesome.com/78b1d29e85.js",
+  "/pages/fallback.html"
 ];
 
-// Install Service Worker
+// Cache size limit function
+const limitCacheSize = (name, size) => {
+  caches.open(name).then(cache => {
+    cache.keys().then(keys => {
+      if (keys.length > size) {
+        cache.delete(keys[0]).then(limitCacheSize(name, size));
+      }
+    });
+  });
+};
+
+// Install service worker
 self.addEventListener("install", evt => {
-  // Console log to instruct that sw.js has been installed
-  //   console.log('service worker has been installed');
+  // console.log("service worker has been installed");
   evt.waitUntil(
     caches.open(staticCacheName).then(cache => {
       console.log("caching shell assets");
@@ -35,16 +41,15 @@ self.addEventListener("install", evt => {
   );
 });
 
-// Listen for Service Worker Activation
+// Activate event
 self.addEventListener("activate", evt => {
-  // Console log to intercept fetch resource request
-  //   console.log("service worker has been activated");
+  // console.log("service worker has been activated");
   evt.waitUntil(
     caches.keys().then(keys => {
-      //   console.log(keys);
+      // console.log(keys);
       return Promise.all(
         keys
-          .filter(key => key !== staticCacheName)
+          .filter(key => key !== staticCacheName && key !== dynamicCacheName)
           .map(key => caches.delete(key))
       );
     })
@@ -53,20 +58,27 @@ self.addEventListener("activate", evt => {
 
 // Fetch event
 self.addEventListener("fetch", evt => {
-  // Console log to intercept fetch resource request
-  //   console.log('fetch event', evt);
-  evt.respondWith(
-    caches.match(evt.request).then(cacheRes => {
-      // Return from cache is item is in assets, otherwise continue with normal fetch request
-      return (
-        cacheRes ||
-        fetch(evt.request).then(fetchRes => {
-          return caches.open(dynamicCache).then(cache => {
-            cache.put(evt.request.url, fetchRes.clone());
-            return fetchRes;
-          });
+  if (evt.request.url.indexOf("firestore.googleapis.com") === -1) {
+    evt.respondWith(
+      caches
+        .match(evt.request)
+        .then(cacheRes => {
+          return (
+            cacheRes ||
+            fetch(evt.request).then(fetchRes => {
+              return caches.open(dynamicCacheName).then(cache => {
+                cache.put(evt.request.url, fetchRes.clone());
+                limitCacheSize(dynamicCacheName, 15);
+                return fetchRes;
+              });
+            })
+          );
         })
-      );
-    })
-  );
+        .catch(() => {
+          if (evt.request.url.indexOf(".html") > -1) {
+            return caches.match("/pages/fallback.html");
+          }
+        })
+    );
+  }
 });
